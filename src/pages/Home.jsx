75a07e20f386dbcaa31081143840d45ea7153bbe
Rahil <raheelkhatttal@gmail.com>
2023-11-27 16:31:40 +0530

    React router dom setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,16 @@ import ButtonGreen from "../components/ButtonGreen";
 const Home = () => {
   return (
     <>
-      <div class="container home-container">
-        <div class="row heading">
-          <div class="col">
-            <h1 class="home-heading">How do you want to use Eco Pulse?</h1>
-            <div class="d-grid gap-2 col-6 button-group">
+      <div className="container home-container">
+        <div className="row heading">
+          <div className="col">
+            <h1 className="home-heading">How do you want to use Eco Pulse?</h1>
+            <div className="d-grid gap-2 col-6 button-group">
               <ButtonGreen btnText="User" />
               <ButtonGreen btnText="Agency/Company" />
             </div>
           </div>
-          <div class="col home-img-container">
+          <div className="col home-img-container">
             <img src={Pulse} alt="logo" />
           </div>
         </div>
