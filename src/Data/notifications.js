2780018cf90731448e646c0d7e59a85bd55355b8
Rahil <raheelkhatttal@gmail.com>
2023-11-29 12:49:25 +0530

    Add dummy data for notifications

---
+++ src/Data/notifications.js
@@ -0,0 +1,21 @@
+export const Notifications = [
+  {
+    date: "Sunday, November 19, 2023 5:22 PM",
+    message:
+      "You have Placed order on 2023-11-21 at 3-5 PM to company1 for the service of random",
+  },
+  {
+    date: "Sunday, November 19, 2023 5:18 PM",
+    message:
+      "You have Placed order on 2023-11-22 at 5-7 PM to company1 for the service of random",
+  },
+  {
+    date: "Friday, November 17, 2023 9:31 AM",
+    message:
+      "You have Placed order on 2023-11-22 at 1-3 PM to company2 for the service of Electronics Waste",
+  },
+  {
+    date: "Sent at: Wednesday, November 15, 2023 11:51 AM",
+    message: "You have request for Metal on 2023-11-15 at 1-3 PM from company2",
+  },
+];
