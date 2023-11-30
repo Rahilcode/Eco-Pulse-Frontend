23e86a9d5b463c4b604e41185a2a7bf719cbb871
Rahil <raheelkhatttal@gmail.com>
2023-11-30 14:55:57 +0530

    Service card component created

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const ServiceCard = ({ service, flag, onDelete }) => {
+  const [rate, setRate] = useState([]);
+  useEffect(() => {
+    let newrate = [];
+    for (let i = 1; i <= service.ratings; i++) {
+      newrate.push(i);
+    }
+    setRate(newrate);
+    console.log(rate);
+  }, []);
+
+  return (
+    <div className="card w-75 mb-3 bg-light">
+      <div className="card-body p-1">
+        <div className="row">
+          <div className="col-md col-lg-3 d-flex justify-content-center align-items-center">
+            <img
+              src={service.image}
+              alt="service logo"
+              height={160}
+              width={180}
+            />
+          </div>
+
+          <div className="col-md">
+            <div className="d-md-flex justify-content-between">
+              <h2 className="card-title m-0">{service.title}</h2>
+              <h2 className="card-title m-0">
+                <span className="badge rounded bg-dark price-tag">
+                  ₹ {service.price}
+                </span>
+              </h2>
+            </div>
+
+            <h5 className="card-text text-secondary">{service.companyName}</h5>
+            <div className="d-flex gap-2">
+              <h4>
+                <span className="badge bg-black"> {service.city}</span>
+              </h4>
+            </div>
+
+            <div className="d-flex justify-content-between">
+              <h4 className="card-text text-secondary service-type">
+                Type: {service.type}
+              </h4>
+
+              {service.ratings === 0 ? (
+                <div>
+                  <h5 className="card-text col-md">Ratings: 0</h5>
+                </div>
+              ) : (
+                <div>
+                  <span className="card-text col-md"> Ratings: </span>
+                  {rate.map((ele, index) => {
+                    return (
+                      <i
+                        key={index}
+                        className="fa-solid fa-star fa-lg my-1"
+                        style={{ color: "#ffc800" }}
+                      ></i>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+
+            {flag && (
+              <div className="d-flex justify-content-between align-items-center">
+                <div>
+                  <span className="visually-hidden">3</span>
+                  <div class="ms-5 col-md my-2 d-flex"></div>
+                </div>
+                {flag == "true" ? (
+                  <div className="col col-2 my-2">
+                    <Link
+                      to="/order"
+                      className="btn button-green fw-bold ms-lg-4"
+                    >
+                      Order
+                    </Link>
+                  </div>
+                ) : (
+                  <div className="col text-end me-1">
+                    <Link
+                      to="/company/services/edit"
+                      className="btn btn-warning fw-bold me-2"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      className="btn btn-danger fw-bold"
+                      onClick={() => onDelete(service.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
---
+++ src/constants/services.js
@@ -0,0 +1,35 @@
+import About from "../images/about.jpeg";
+import Eco from "../images/eco.avif";
+import Ecco from "../images/eco.jpeg";
+export const ServicesData = [
+  {
+    id: "1",
+    title: "Electronics Waste",
+    companyName: "Company2",
+    city: "Bangalore",
+    type: "Electronics",
+    ratings: 4,
+    price: "200",
+    image: Eco,
+  },
+  {
+    id: "2",
+    title: "Service1",
+    companyName: "Company1",
+    city: "Mumbai",
+    type: "Bio Degradable",
+    ratings: 3,
+    price: "500",
+    image: About,
+  },
+  {
+    id: "3",
+    title: "Service3",
+    companyName: "Company1",
+    city: "Chennai",
+    type: "Electronics",
+    ratings: 2,
+    price: "100",
+    image: Ecco,
+  },
+];
