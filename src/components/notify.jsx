f5c3fe5704a5e808914bab090c029a2e083937ae
Rahil <raheelkhatttal@gmail.com>
2023-11-30 11:20:23 +0530

    Modify notify components

--- src/components/notify.jsx
+++ src/components/notify.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Notify = ({ date, message }) => {
   return (
-    <li class="bg-primary text-light fw-bold p-2 pb-1 m-2 border rounded">
+    <li className="bg-primary text-light fw-bold p-2 pb-1 m-2 border rounded">
       <span>Sent at: {date}</span>
       <p>{message}</p>
     </li>
